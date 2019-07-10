b0a5223214abc0e0e9b626faaece91c8a906a4e9
Soleyu <soleyu.alpha@gmaill.com>
2019-07-10 00:54:22 -0500

    SPrites and first code
    Added Sprites and First Code  for chickenmovement. HGad to do it from scracth

---
+++ space-chicken-wbit/Assets/SpaceChickenController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceChickenController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    private bool isOver = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOver == false)
+        {
+        float moveHorizontal = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+        float moveVertical = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + moveHorizontal, transform.position.y + moveVertical);
+      
+        }
+    }
+}
